57cafc26986a7d36188aaaab0a9cbb543b64cf2e
Kooroush Masoumi <kooroushmasoumi@gmail.com>
2026-05-17 19:04:38 +0330

    write readme quickstart

--- tests/test_core.py
+++ tests/test_core.py
@@ -114,3 +114,5 @@ def test_typed_accessors(base_file):
 
 def test_wrong_type_raises(base_file):
     loader = ConfigLoader().load_file(base_file)
+    with pytest.raises(ConfigError):
+        loader.require_int("app.name")
